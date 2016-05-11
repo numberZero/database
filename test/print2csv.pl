6052b183b3e98144267d34161005936aa5ed981e
number Zero <silverunicorn2011@yandex.ru>
2016-05-11 21:16:05 +0300

    Program output to CSV converter works

---
+++ test/print2csv.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+sub readitem { # reads into global %row
+	my $name = shift(@_);
+	my $sep = shift(@_) || ",";
+	$_ = <> or die "Can't read from the file: $!\n";
+	m/^$name: (.+)$/ or die "Unexpected item: $_\n";
+	$row{$name} = $1;
+	print "$1$sep";
+}
+
+while(<>) {
+	if(m/\*\*\* Row (\d+) \*\*\*\n$/) { # row start
+		%row = ();
+#		print "Row $1: ";
+		readitem "teacher";
+		readitem "subject";
+		readitem "room";
+		readitem "group";
+		readitem "day";
+		readitem "lesson", "\n";
+		<> == "" or die "Empty line expected\n";
+	}
+	elsif(m/^\*\*\* END \*\*\*\n$/) {
+		break;
+	}
+}
